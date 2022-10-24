6b7e1c107d20b1ef33b4a67388a04dd61cd8b391
FranciscoCardita <franciscocardita@ua.pt>
2022-10-24 17:03:38 +0100

    Add rsa encryption

---
+++ AsymmetricCryptography/rsa_encryption.py
@@ -0,0 +1,31 @@
+import argparse
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import serialization
+
+def main(public_key):
+        
+    with open(public_key, 'rb') as input_file:
+        pub_key = serialization.load_pem_public_key(
+            input_file.read(),
+        )
+
+    message = b"encrypted data"
+    ciphertext = pub_key.encrypt(
+        message,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    print(ciphertext)
+
+    with open("rsa_encrypted.txt", 'wb') as output:
+        output.write(ciphertext)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-pk", "--public_key", required=True, help="<file_name>.pem")
+    public_key = vars(parser.parse_args())["public_key"]
+    main(public_key)
