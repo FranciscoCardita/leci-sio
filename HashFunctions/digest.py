b5ec0c2011fd5acd0696b1ae10574203a8b2a9f8
FranciscoCardita <franciscocardita@ua.pt>
2022-10-31 14:50:03 +0000

    Add hash functions

---
+++ HashFunctions/digest.py
@@ -0,0 +1,25 @@
+from cryptography.hazmat.primitives import hashes
+import os
+import sys
+import binascii
+
+def calculate(fname):
+    digest = hashes.Hash(hashes.SHA256())
+
+    with open(fname, 'rb') as input_file:
+        while data := input_file.read(1024*1024):
+            digest.update(data)
+    
+    return digest.finalize()
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python digest.py <file_name>")
+
+    for fname in sys.argv[1:]:
+        hd = binascii.hexlify(calculate(fname))
+        print(f'{hd.decode()}: {fname}')
+
+if __name__ == '__main__':
+    main()
+
